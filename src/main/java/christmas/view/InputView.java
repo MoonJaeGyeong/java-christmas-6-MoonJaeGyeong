3006bde3058db086d9fea6286f71643657254e9c
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-13 18:02:30 +0900

    <feat> : <사용자의 입력을 받는 Inputview 클래스 구현>

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,22 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.view.constant.ConstantMessage;
+
+public class InputView {
+    public static String RequestDayByUserInput(){
+        printConstantMessage(ConstantMessage.REQUEST_DAY);
+        return Console.readLine();
+    }
+
+    public static String RequestMenuByUserInput(){
+        printConstantMessage(ConstantMessage.REQUEST_MENU);
+        return Console.readLine();
+    }
+    private static void printNewLine() {
+        System.out.println();
+    }
+    private static void printConstantMessage(ConstantMessage constantMessage){
+        System.out.println(constantMessage.getMessage());
+    }
+}
