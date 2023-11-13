54375b786afe0c9af8047df3457d99f651b46005
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-13 23:40:44 +0900

    <feat> : <VisitDay 생성하는 메서드 생성>
    visitday 를 생성하고 유효성 검사가 완료될 때 까지 반복하는 iterator 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,6 +1,8 @@
 package christmas;
 
 import christmas.controller.ChristmasEventController;
+
+import christmas.util.Iterator;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
@@ -8,7 +10,9 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        ChristmasEventController christmasEventController = new ChristmasEventController(inputView, outputView);
+        Iterator iterator = new Iterator(outputView::printErrorMessage);
+        ChristmasEventController christmasEventController =
+                new ChristmasEventController(inputView, outputView, iterator);
 
         christmasEventController.run();
     }
--- src/main/java/christmas/controller/ChristmasEventController.java
+++ src/main/java/christmas/controller/ChristmasEventController.java
@@ -1,17 +1,28 @@
 package christmas.controller;
 
+import christmas.domain.VisitDay;
+import christmas.util.Iterator;
 import christmas.view.InputView;
 import christmas.view.OutputView;
+
 
 public class ChristmasEventController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final Iterator iterator;
 
-    public ChristmasEventController(InputView inputView, OutputView outputView){
+    public ChristmasEventController(InputView inputView, OutputView outputView, Iterator iterator){
         this.inputView = inputView;
         this.outputView = outputView;
+        this.iterator = iterator;
     }
     public void run(){
+        outputView.printGreetings();
+        VisitDay visitDay = getVisitDay();
 
+    }
+
+    private VisitDay getVisitDay(){
+        return iterator.iterate(() -> new VisitDay(inputView.RequestDayByUserInput()));
     }
 }
