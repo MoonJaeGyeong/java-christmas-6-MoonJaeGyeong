32ae292889c0b6d0a33f76ebb358f7ef2b22574f
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-14 03:52:18 +0900

    <feat> : <유저에게 주문을 받은 후 출력이 가능한 부분 구현>

--- src/main/java/christmas/controller/ChristmasEventController.java
+++ src/main/java/christmas/controller/ChristmasEventController.java
@@ -1,6 +1,7 @@
 package christmas.controller;
 
 import christmas.domain.Order;
+import christmas.domain.event.PresentationMenu;
 import christmas.domain.VisitDay;
 import christmas.util.Iterator;
 import christmas.view.InputView;
@@ -22,6 +23,17 @@ public class ChristmasEventController {
 
         VisitDay visitDay = getVisitDay();
         Order order = getOrder();
+
+        printUserOrderAndTotalPriceAndPresentation(order);
+    }
+
+    private void printUserOrderAndTotalPriceAndPresentation(Order order){
+        int total_Price = order.TotalPrice();
+
+        outputView.printOrderMenu(order.getUserOrders());
+        outputView.printTotalPriceBeforeDiscount(total_Price);
+        PresentationMenu presentationMenu = new PresentationMenu(total_Price);
+        outputView.printPresentationMenu(presentationMenu.getPresentation());
     }
 
     private VisitDay getVisitDay(){
