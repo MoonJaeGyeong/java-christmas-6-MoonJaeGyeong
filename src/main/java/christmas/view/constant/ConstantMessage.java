f936ab3fb9f35a22d62fbf10f1646e6319e63fa8
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-13 19:07:50 +0900

    <style> : <프린트 문 수정>

--- src/main/java/christmas/view/constant/ConstantMessage.java
+++ src/main/java/christmas/view/constant/ConstantMessage.java
@@ -3,8 +3,8 @@ package christmas.view.constant;
 public enum ConstantMessage {
 
     EVENT_MONTH("12"),
-    Greetings("안녕하세요! 우테코 식당" + EVENT_MONTH.getMessage() + "월 이벤트 플래너입니다."),
-    REQUEST_DAY(EVENT_MONTH.getMessage() + "월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해주세요!"),
+    Greetings("안녕하세요! 우테코 식당 " + EVENT_MONTH.getMessage() + "월 이벤트 플래너입니다."),
+    REQUEST_DAY(EVENT_MONTH.getMessage() + "월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해주세요!)"),
     REQUEST_MENU("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1"),
     EVENT_INFORMATION("우테코 식당에서 받을 이벤트 혜택 미리보기!"),
     ORDER_MENU("<주문 메뉴>"),
