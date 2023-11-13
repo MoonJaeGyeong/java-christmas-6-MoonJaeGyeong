4fc6f88878d9428a1af46e48e505aba4bbb2621e
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-14 03:52:57 +0900

    <mov> : <PresentaionMenu 파일 위치 이동>

---
+++ src/main/java/christmas/domain/event/PresentationMenu.java
@@ -0,0 +1,26 @@
+package christmas.domain.event;
+
+public class PresentationMenu {
+    private final String presentation;
+    private static final String ONE_CHAMPAGNE = "샴페인 1개";
+    private static final String NOTHING = "없음";
+    private static final int MINIMUM_PRICE_PRICE_FOR_PRESENTATION = 120000;
+
+    public PresentationMenu(int total_Price){
+        this.presentation = SelectPresentationByDiscountPrice(total_Price);
+    }
+    public String getPresentation(){
+        return presentation;
+    }
+
+    public boolean isPresentation(){
+        return !presentation.equals("없음");
+    }
+
+    private String SelectPresentationByTotalPrice(int total_Price){
+        if(total_Price < MINIMUM_PRICE_PRICE_FOR_PRESENTATION){
+            return NOTHING;
+        }
+        return ONE_CHAMPAGNE;
+    }
+}
