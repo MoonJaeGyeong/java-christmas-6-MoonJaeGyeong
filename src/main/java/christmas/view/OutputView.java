65f59e4a4f3e40dfc6e3c2d61eaba51aef0aa2a9
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-15 00:17:17 +0900

    <feat> : <혜택 내역 출력 부분을 상세히 구현>

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,17 +2,16 @@ package christmas.view;
 
 import christmas.view.constant.ConstantMessage;
 import java.text.NumberFormat;
-import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
 public class OutputView {
     public static final NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.KOREA);
-
-    private final static String PRESENTATION_NUMBER = "1";
+    
     private final static String SPACE = " ";
     private final static String EA = "개";
     private final static String WON = "원";
+    private final static String NOTHING = "없음";
 
 
     public void printErrorMessage(String errorMessage){
@@ -23,6 +22,7 @@ public class OutputView {
     }
 
     public static void printOrderMenu(Map<String, Integer> order_Menu){
+        printNewLine();
         printConstantMessage(ConstantMessage.ORDER_MENU);
         for(String menu : order_Menu.keySet()){
             System.out.println(menu + SPACE + order_Menu.get(menu).toString() + EA);
@@ -32,38 +32,52 @@ public class OutputView {
 
     public static void printTotalPriceBeforeDiscount(int price){
         printConstantMessage(ConstantMessage.TOTAL_PRICE_BEFORE_DISCOUNT);
-        printPriceAfterFormating(price);
+        System.out.println(PriceAfterFormating(price) + WON);
         printNewLine();
     }
 
     public static void printPresentationMenu(String presentation){
         printConstantMessage(ConstantMessage.PRESENTATION_MENU);
         System.out.println(presentation);
+        printNewLine();
     }
 
-    public static void printBenefitDetails(){
+    public static void printBenefitInfo(Map<String, Integer> BenefitInfo){
         printConstantMessage(ConstantMessage.BENEFIT_DETAILS);
+        for(String discountName : BenefitInfo.keySet()){
+            if(discountName.equals(NOTHING)){
+                System.out.println(NOTHING);
+            }
+            if(!discountName.equals(NOTHING)){
+                String amount = PriceAfterFormating(BenefitInfo.get(discountName));
+                System.out.println(discountName + amount + WON);
+            }
+        }
+        printNewLine();
     }
 
     public static void printTotalBenefitPrice(int price){
         printConstantMessage(ConstantMessage.TOTAL_BENEFIT_PRICE);
-        printPriceAfterFormating(price);
+        System.out.println(PriceAfterFormating(price) + WON);
+        printNewLine();
     }
 
     public static void printTotalPriceAfterDiscount(int price){
         printConstantMessage(ConstantMessage.TOTAL_PRICE_AFTER_DISCOUNT);
-        printPriceAfterFormating(price);
+        System.out.println(PriceAfterFormating(price) + WON);
+        printNewLine();
     }
 
     public static void printEventBadge(String event_Badge){
         printConstantMessage(ConstantMessage.EVENT_BADGE);
         System.out.println(event_Badge);
+        printNewLine();
     }
 
-    private static void printPriceAfterFormating(int price){
+    private static String PriceAfterFormating(int price){
         String amount = numberFormat.format(price);
         amount = amount.replace("₩", "");
-        System.out.println(amount + WON);
+        return amount;
     }
 
     private static void printConstantMessage(ConstantMessage constantMessage){
